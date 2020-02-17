98c7e97fc36245ef828cce4827352ead50a35b05
Gleb Shidkov <gshidkov@slb.com>
2020-02-18 01:01:33 +0300

    feat: add match all example

---
+++ code-examples/match-all.js
@@ -0,0 +1,7 @@
+export function matchAllExample() {
+    return [...'hello1 hello2'.matchAll(/hello(\d)/g)];
+}
+
+export function matchAllExample() {
+    return 'hello1 hello2'.match(/hello(\d)/g);
+}
