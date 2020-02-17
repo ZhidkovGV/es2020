05c7ec52b424c4893fbee2ed2eddf3769a19e0a8
Gleb Shidkov <gshidkov@slb.com>
2020-02-18 01:20:16 +0300

    feat: add index files

---
+++ code-examples/index.js
@@ -0,0 +1,10 @@
+import { bigIntExample } from './bigint.js';
+import { nullishExample } from './nullish-operator.js';
+import { dynamicImport } from './dynamic-import/dynamic-import.js';
+import { worker, browserGlobalThis } from './global-this/global-this.js';
+import { ClassWithPrivateProperty } from './private-class-members.js';
+import { SomeClass } from './class-members.js';
+import { allMethodExample,  matchAllExample} from './match-all.js';
+import { numericLiteralExample } from './numeric-literals.js';
+import { promiseAllSettledExample, promiseAllExample } from './all-settled.js';
+import { optionalChainExample, opionalChainWithNullish } from './optional-chain.js';
